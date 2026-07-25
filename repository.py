afbcf1f18a9f06197aaa1792e77fbec8862520e1
Elnoe <asausat@gmail.com>
2026-07-25 13:41:34 +0330

    Prepare repository for database versioning
    - Separate V1 and V2 database paths
    - Add repository support for normalized database
    - Keep existing application behavior unchanged

--- repository.py
+++ repository.py
@@ -2,23 +2,25 @@ import os
 import json
 
 
-DATABASE_PATH = "data/database.json"
-print("Repository file:", DATABASE_PATH)
+DATABASE_V1 = "data/database.json"
+DATABASE_V2 = "data/database_v2.json"
+print("Repository file:", DATABASE_V1)
 
 class JSONRepository:
+    
     def exists(self):
 
-        return os.path.exists(DATABASE_PATH)
+        return os.path.exists(DATABASE_V1)
 
     def load(self):
-        print("LOAD:", DATABASE_PATH)
-        with open(DATABASE_PATH, "r", encoding="utf-8") as f:
+        
+        with open(DATABASE_V1, "r", encoding="utf-8") as f:
 
             return json.load(f)
 
     def save(self, data):
-        print("SAVE:", DATABASE_PATH)
-        with open(DATABASE_PATH, "w", encoding="utf-8") as f:
+        
+        with open(DATABASE_V1, "w", encoding="utf-8") as f:
 
             json.dump(
                 data,
@@ -27,5 +29,17 @@ class JSONRepository:
                 indent=4
             )
 
+    def load_v2(self):
+        with open(DATABASE_V2, "r", encoding="utf-8") as f:
+            return json.load(f)
+
+    def save_v2(self, data):
+        with open(DATABASE_V2, "w", encoding="utf-8") as f:
+            json.dump(
+                data,
+                f,
+                ensure_ascii=False,
+                indent=4
+            )
 
 repository = JSONRepository()
